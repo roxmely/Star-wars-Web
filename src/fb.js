fd707aef79815f12a0c7704442b95dfc65026238
roxana melgarejo <r.melgarejo.ubiqum@gmail.com>
2020-05-13 15:44:00 +0200

    fb

---
+++ src/fb.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase;
+
+export default db;
+export const database = firebase.firestore();
